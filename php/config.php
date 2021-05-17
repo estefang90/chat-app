d037b5cf342b928cb327139f8cb4e8d91d37e58d
estefang90 <36801483+estefang90@users.noreply.github.com>
2021-05-16 23:08:34 -0500

    chat

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php
   $hostname = "localhost";
-  $username = "systemsg_pablogiron";
-  $password = "giron10pablo";
-  $dbname = "systemsg_chat";
+  $username = "root";
+  $password = "";
+  $dbname = "chatapp";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
   if(!$conn){
